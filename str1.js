54cb30f7219d73e4486492d5aebcf31e9ce1a9e8
Admin <vanpho02@gmail.com>
2017-03-25 19:41:01 +0700

    concat

---
+++ str1.js
@@ -0,0 +1,11 @@
+const str = 'Khoa Pham';
+const str2 = "Khoa Pham";
+
+const str3 = 'Khoa " Pham';
+const str4 = `Khoa ' " Pham`;
+
+const firstName = 'Khoa';
+const lastName = 'Pham';
+
+console.log('Chao ' + firstName + ' ' + lastName + '!');
+console.log(`Chao ${firstName} ${lastName}!`);
